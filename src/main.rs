282004b0e424748d1024ade6d7395f26fecb61b7
Chris geihsler <chris@geihsler.net>
2019-03-17 14:14:20 -0400

    Getting port from environment.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use actix_web::server;
+use std::env;
+use std::net::SocketAddr;
 
 mod insults;
 mod web;
 
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8080)
+}
+
 fn main() {
+    let addr = SocketAddr::from(([0, 0, 0, 0], get_server_port()));
+
     server::new(|| {
         let config = insults::Config::from_files();
         web::build_app(config)
     })
-    .bind("127.0.0.1:8088")
+    .bind(addr)
     .unwrap()
     .run()
 }
